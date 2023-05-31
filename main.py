25f97f5fd589c2ef004b5ed300e5e50aed7c0b44
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2023-05-30 23:57:29 -0300

    encontrando a posição das torres

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ with open("input.txt", "r") as f:
     mapa = arquivo[1:]
     f.close()
 
+indices = []
+for linha in range(len(mapa)):
+    indices.append([indice for indice, elemento in enumerate(mapa[linha]) if elemento == "T"])
+
 with open("output.txt", "w") as f:
     for linha in mapa:
+        f.write(" ".join(linha) + "\n")
+    for linha in indices:
         f.write(" ".join(linha) + "\n")
     f.close()
 
